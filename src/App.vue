444a6f560bab02886d643be80ba4f3b641628459
Zen <muhzaini30@gmail.com>
2022-06-14 07:01:25 +0800

    Alpine JS selesai

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
   <router-view />
 </template>
+
+<style>
+  [x-cloak]{
+    @apply hidden
+  }
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,6 @@
 <template>
-	<div class='p-5'>
-		<h1 class='text-3xl text-zinc-500 mb-1'>Assalamualaikum</h1>
-		<p class="text-zinc-500">Welcome to this Vue template, cooked by Zen.</p>
+	<div x-data='{open: false}' x-cloak class="p-6">
+		<button x-on:click='open = !open' class="bg-black text-white px-5 py-2 rounded-lg">toggle</button>
+		<div x-show='open' class="mt-4">hello</div>
 	</div>
-
-	<img v-for='(_, n) in Array(5)' class="block mb-10 w-full h-auto" :src='`https://http.cat/${n + 1}00.jpg`' alt="" />
 </template>
-
-<style scoped="">
-	* {
-		@apply italic text-center
-	}
-</style>
